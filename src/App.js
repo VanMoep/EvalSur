00cd16fcba9b687e0c541e872ddbe08f7f54991e
Tobi F <tobias.faass@gmail.com>
2021-09-04 15:45:20 +0200

    added multipe TileLayers

--- src/App.js
+++ src/App.js
@@ -138,19 +138,50 @@ class App extends React.Component {
           zoom={this.state.defaultZoom}
         >
           <LayersControl position="topright">
-            <LayersControl.BaseLayer checked name="OpenStreetMap.Mapnik">
+            <LayersControl.BaseLayer checked name="CartoDB.VoyagerLabelsUnder">
+              <TileLayer
+                url='https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}{r}.png'
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+              />
+            </LayersControl.BaseLayer>
+            <LayersControl.BaseLayer checked name="CartoDB.Positron">
+              <TileLayer
+                url='https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png'
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+                subdomains='abcd'
+              />
+            </LayersControl.BaseLayer>
+            <LayersControl.BaseLayer name="OpenStreetMap.BZH">
+              <TileLayer
+                url="https://tile.openstreetmap.bzh/br/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles courtesy of <a href="http://www.openstreetmap.bzh/" target="_blank">Breton OpenStreetMap Team</a> Data mining by [<a href="http://overpass-api.de/">Overpass API</a>]'
+              />
+            </LayersControl.BaseLayer>
+            <LayersControl.BaseLayer name="OpenStreetMap.Mapnik">
               <TileLayer
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> 
             Data mining by [<a href="http://overpass-api.de/">Overpass API</a>]'
               />
             </LayersControl.BaseLayer>
-            <LayersControl.BaseLayer checked name="OpenStreetMap.Mapnik">
+            <LayersControl.BaseLayer name="Stamen.Toner">
               <TileLayer
                 url="https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}"
-                attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors Data mining by [<a href="http://overpass-api.de/">Overpass API</a>]'
                 subdomains='abcd'
                 ext='png'
+              />
+            </LayersControl.BaseLayer>
+            <LayersControl.BaseLayer name="Stadia.AlidadeSmooth">
+              <TileLayer
+                url='https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png'
+                attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+              />
+            </LayersControl.BaseLayer>
+            <LayersControl.BaseLayer name="Stadia.AlidadeSmoothDark">
+              <TileLayer
+                url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+                attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
               />
             </LayersControl.BaseLayer>
           </LayersControl>
@@ -160,7 +191,7 @@ class App extends React.Component {
           </Control>
         </Map>
 
-      </div>
+      </div >
     );
   }
 }
