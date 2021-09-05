b4142ef6822de65871bb7531a533cfdae53cda5d
Tobi F <tobias.faass@gmail.com>
2021-09-05 16:17:43 +0200

    Updates

---
+++ precache-manifest.3fa3cb9f5a0ad52f6587ed415342a4b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0afd27a4652c5bd536e7e6006edb8f7",
+    "url": "/EvalSur/index.html"
+  },
+  {
+    "revision": "72acc4d8674ec1a1e269",
+    "url": "/EvalSur/static/css/2.b06da25b.chunk.css"
+  },
+  {
+    "revision": "96486158e64e5f7da280",
+    "url": "/EvalSur/static/css/main.52e75b73.chunk.css"
+  },
+  {
+    "revision": "72acc4d8674ec1a1e269",
+    "url": "/EvalSur/static/js/2.b2e11155.chunk.js"
+  },
+  {
+    "revision": "9b507485cdd456fcbdbb0090739d0dd5",
+    "url": "/EvalSur/static/js/2.b2e11155.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96486158e64e5f7da280",
+    "url": "/EvalSur/static/js/main.d5286c72.chunk.js"
+  },
+  {
+    "revision": "444bb1089c811890365f",
+    "url": "/EvalSur/static/js/runtime-main.3d77540c.js"
+  }
+]);
